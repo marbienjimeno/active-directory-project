9d229069780fe9bee836c05d1b4016378c638af0
Marbien Jimeno <jmarbien@gmail.com>
2022-08-08 23:37:05 -0700

    Added comments

--- NewUsersCsv.ps1
+++ NewUsersCsv.ps1
@@ -1,30 +1,33 @@
-﻿# Import the AD Module
+# Import the AD Module
 Import-Module ActiveDirectory
 
-# Default password for new users 
+# Default password for all new users 
 $DEFAULT_PASS = "Password1234!"
 
 # Get the path for the target CSV file
 $filepath = Read-Host -Prompt "Enter path to CSV file containing New Users"
 
-# Import CSV data as array
+# Import users from CSV file as array
 $users = Import-CSV $filepath
 
-# For each user in the CSV file
+# For each user in the extracted from the CSV file
 ForEach ($user in $users) {
-    # Make first and last names lower-cased
+    # Make first and last names all lower-cased
     $firstName = $user.'First Name'.ToLower()
     $lastName = $user.'Last Name'.ToLower()
     
-    # Formats username based on full name 
+    # Format username based on full name. Ex. Full Name: Marbien Jimeno -> Username: mjimeno
     $username = "$($firstName.Substring(0,1))$($lastName)"
 
-    # Adds new user to active directory
+    # Add new user to active directory
     New-ADUser `
+        # Enters values of new user based on data from CSV file
         -Name ($user.'First Name' + " " + $user.'Last Name') `
         -GivenName $user.'First Name' `
         -Surname $user.'Last Name' `
+        # Previously formatted username object is used
         -UserPrincipalName $username `
+        # The variable holding the default password is used and encryption is performed
         -AccountPassword (ConvertTo-SecureString $DEFAULT_PASS -AsPlainText -Force) `
         -Description $user.Description `
         -EmailAddress $users.'Email Address' `
@@ -32,5 +35,7 @@ ForEach ($user in $users) {
         -OfficePhone $user.Phone `
         -Path $user.'Organizational Unit' `
         -Enabled ([System.Convert]::ToBoolean($user.Enabled)) `
+        # The user does not have the change their password at next logon. This is normally
+        # set to true but, for lab purposes, the feature is turned off
         -ChangePasswordAtLogon 1
 }
