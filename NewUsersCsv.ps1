a8855bdfe0f512c7ba37e72f9ccbf97646926a74
Marbien Jimeno <jmarbien@gmail.com>
2022-08-11 16:49:06 -0700

    Updated NewUsersFinal.csv

--- NewUsersCsv.ps1
+++ NewUsersCsv.ps1
@@ -27,7 +27,7 @@ ForEach ($user in $users) {
         -UserPrincipalName $username `
         -AccountPassword (ConvertTo-SecureString $DEFAULT_PASS -AsPlainText -Force) `
         -Description $user.Description `
-        -EmailAddress $users.Email `
+        -EmailAddress $user.Email `
         -Title $user.'Job Title' `
         -OfficePhone $user.Phone `
         -Path $user.'Organizational Unit' `
